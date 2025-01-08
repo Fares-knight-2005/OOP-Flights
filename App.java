0d65b7537e6afaa08f93fbba2defe32912164426
Fares <fares.oyion123@gmail.com>
2025-01-08 22:33:21 +0300

    The main

---
+++ App.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+
+        MainMenu.MainMenu();
+
+/* 
+         try{
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Pasengers.txt"));
+        writer.write("Hello I am Fares Writing From the Code :");
+        writer.write("\nhello Again Dont Forget me\n"); 
+        writer.close();
+        } catch(Exception e){
+        e.printStackTrace();
+        }
+
+       BufferedReader Reader = new BufferedReader(new FileReader("Pasengers.txt"));
+
+
+       String Line = Reader.readLine();
+
+
+       Reader.close();
+
+
+       System.out.println(Line);
+*/
+    }
+}
