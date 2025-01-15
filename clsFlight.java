c3a23a10d49c75c78be58b352f581a964327bc1b
Fares <fares.oyion123@gmail.com>
2025-01-15 14:13:21 +0300

    Flights Class

--- clsFlight.java
+++ clsFlight.java
@@ -1,3 +1,20 @@
 public class clsFlight {
 
+    private int NumberOfChars;
+    private int NumberOfPasengers;
+    private String NumberOfFlight;
+    private String DateToTravel;
+    private String DateToGoBack;
+    private int price;
+    private String FromCountry;
+    private String ToCountry;
+    private String StringTimeOfGo;
+    private String TimeOfArriving;
+    private String HowMuchTimeInTheSky;
+
+    public int GetNumberOfChars()
+    {
+        return NumberOfChars;
+    }
+
 }
