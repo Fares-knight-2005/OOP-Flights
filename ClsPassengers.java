92c9e020ceb678d8e683d779c91a28d1b0f4f66e
Moath shaheen <thedarkx1man@gmail.com>
2025-01-18 23:18:06 +0300

    new additions to passenger class

--- ClsPassengers.java
+++ ClsPassengers.java
@@ -14,7 +14,7 @@ public class ClsPassengers {
  private String PassengerName ;
  private int PassengerAge;
  private int PassengerCash ;
- private int pasword ;
+ private String pasword ;
  String [] arr ;
 
   
@@ -43,10 +43,22 @@ public class ClsPassengers {
   
   return name;
   }
- //هاد هو الباني عملت الو اكثر من وضيفة 
+
+  //هاد باني فارغ يعني بيعطي قيم صفر لل انتجر و فراغ لل سترينغ
+  public ClsPassengers(){
+  
+   PassengerUserName="";
+   PassengerName="";
+   pasword="";
+   PassengerAge=0;
+   PassengerCash=0;
+  }
+  
+   //هاد هو الباني عملت الو اكثر من وضيفة 
   //هو رح ياخد القيم ويخزن القيم داخل ملف بعد تحويلهن الى متحولات من نوع سترينغ
+  
   ClsPassengers(String PassengerUserName,String PassengerName ,int PassengeAge
-                 ,int PassengeCash, int pasword){
+                 ,int PassengeCash, String pasword){
   
   
   PassengerUserName=checkname(PassengerUserName);
@@ -57,7 +69,7 @@ public class ClsPassengers {
   this.PassengerCash=PassengeCash;
   this.pasword=pasword;
   
-  String [ ]arr={Integer.toString(pasword),Integer.toString(PassengerCash),Integer.toString(PassengerAge)};
+  String [ ]arr={Integer.toString(PassengerCash),Integer.toString(PassengerAge)};
  
  try{
  BufferedWriter write = new BufferedWriter (new FileWriter ("user.txt",true));
@@ -70,7 +82,112 @@ public class ClsPassengers {
  }
   }
  
-
   
+  
+  //هون الستير و الجيتر مافيهن شي مميز 
+public void SetPassengerUserName(String passengeruserName){
+this.PassengerUserName=passengeruserName;
+}
+public void SetPassengerName(String passengerName){
+this.PassengerName=passengerName;
+}
+public void SetPassengerAge(int age){
+this.PassengerAge=age;
+}
+public void SetPassengerCash(int cach){
+this.PassengerCash=cach;
+}
+public void Setpasword(String pasword){
+this.pasword=pasword;
+}
+public String  GetPassengerUserName(){
+    return PassengerUserName;
+}
+public String GetpassengerName(){
+return PassengerName;
+}
+public int GetpassengerAge(){
+return PassengerAge ;
+}
+public int Getpassengercach (){
+ return   PassengerCash;
+}
+public String Getpasword (){
+return pasword ;
+}
+
+
+
+
+private static String ConvertToLine(ClsPassengers passenger){
+   
+    String []arr = {passenger.PassengerUserName,passenger.PassengerName
+            ,Integer.toString(passenger.PassengerAge),Integer.toString(passenger.PassengerCash),passenger.pasword};
+     return String.join("#//#", arr);
+
+}
+
+
+public static void AddNew(ClsPassengers passenger){
+try{
+ BufferedWriter write = new BufferedWriter (new FileWriter ("user.txt",true));
+ 
+ write.write(ConvertToLine(passenger));
+ 
+ write.close();
+ }catch(Exception e){
+ e.fillInStackTrace();
+ }
+}
+
+public static ClsPassengers GetEmptyobject (){
+ClsPassengers passenger = new passenger.ClsPassengers();
+return passenger ;
+}
+
+
+
+private static ClsPassengers ConvertLineToObject(String line ){
+
+    ClsPassengers passenger=new ClsPassengers();
+    String [] arr=line.split("#//#");
+    passenger.PassengerUserName=arr[0];
+    passenger.PassengerName=arr[1];
+    passenger.PassengerAge=Integer.parseInt(arr[2]);
+    passenger.PassengerCash=Integer.parseInt(arr[3]);
+    passenger.pasword=arr[4];
+    return passenger;
+
+    }
+
+
+
+public void print (ClsPassengers passenger){
+
+    System.out.println(passenger.PassengerUserName+"\n"+passenger.PassengerName+"\n"+passenger.PassengerAge
+    +"\n"+passenger.PassengerCash+"\n"+pasword);
+}
+
+
+
+public static ArrayList<ClsPassengers> GetAllPassenger(){
+  
+    String line="";
+    ArrayList<ClsPassengers>allpassnger=new ArrayList<>();
+    try{
+ 
+        BufferedReader read =new BufferedReader(new FileReader("user.txt"));
+        while((line=read.readLine())!=null){
+            
+            allpassnger.add(ConvertLineToObject(line));
+            
+        }
+        
+ }catch(Exception e){
+ e.fillInStackTrace();
+ }
+    return allpassnger;
+}
+
 }
 
